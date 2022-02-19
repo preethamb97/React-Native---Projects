3722ee5a8a4ddee2803192c032850f6be042c400
preetham.b <preetham.b@capillarytech.com>
2022-02-20 01:35:27 +0530

    map changes

--- uber-app-learn/App.js
+++ uber-app-learn/App.js
@@ -8,9 +8,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import MapScreen from './screens/MapScreen';
 import EatScreen from './screens/EatScreen';
+import { MY_KEY } from '@env';
 // Redux
 
 export default function App() {
+  console.log(MY_KEY, '*****')
   const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
@@ -20,17 +22,17 @@ export default function App() {
             <Stack.Screen
               name='HomeScreen'
               component={HomeScreen}
-              options={{headerShown: false, }}
+              options={{ headerShown: false, }}
             />
             <Stack.Screen
               name='MapScreen'
               component={MapScreen}
-              options={{headerShown: false, }}
+              options={{ headerShown: false, }}
             />
             <Stack.Screen
               name='EatScreen'
               component={EatScreen}
-              options={{headerShown: false, }}
+              options={{ headerShown: false, }}
             />
           </Stack.Navigator>
         </SafeAreaProvider>
--- uber-app-learn/screens/MapScreen.js
+++ uber-app-learn/screens/MapScreen.js
@@ -1,11 +1,28 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-
+import MapView, { Marker } from 'react-native-maps';
+import tw from 'tailwind-react-native-classnames';
 const MapScreen = () => {
   return (
-    <View>
-      <Text>MapScreen</Text>
-    </View>
+    <MapView
+      style={tw`flex-1`}
+      mapType="mutedStandard"
+      initialRegion={{
+        latitude: 12.9634,
+        longitude: 77.5855,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    >
+    <Marker
+      coordinate={{
+        latitude: 12.9634,
+        longitude: 77.5855,
+      }}
+      title="Origin"
+      description="Origin description"
+    />
+    </MapView>
   )
 }
 
